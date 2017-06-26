7cc330e69e556557176c8a31fc06e77d78c93931
Jens Dideriksen <Jens Dideriksen@DESKTOP-P36C4GD>
2017-06-26 14:48:17 +0200

    IJob and WorkTime

---
+++ Business_Domain/IJob.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business_Domain
+{
+    class IJob
+    {
+        #region
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DeadLine { get; set; }
+        public bool PriceType { get; set; }
+        public int Price { get; set; }
+        public List<WorkTimes> WorkTimeList { get; set; }
+        #endregion
+
+        public List<WorkTimes> ShowListOfTimes()
+        {
+            return WorkTimeList;
+        }
+        public void ShowPriceType(bool PriceType) { }
+
+
+
+
+
+
+
+
+
+    }
+}
