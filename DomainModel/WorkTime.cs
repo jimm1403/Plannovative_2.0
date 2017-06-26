e17e108af7fe8f99aca4d840cca5263d43369301
Jens Dideriksen <Jens Dideriksen@DESKTOP-P36C4GD>
2017-06-26 20:42:37 +0200

    nye ændringer

--- DomainModel/WorkTime.cs
+++ DomainModel/WorkTime.cs
@@ -7,8 +7,8 @@ namespace DomainModel
 {
     public class WorkTime
     {
-        DateTime StartTime { get; set; }
-        DateTime EndTime { get; set; }
+        TimeSpan StartTime { get; set; }
+        TimeSpan EndTime { get; set; }
         DateTime WorkDate { get; set; }
         float TotalAmountOfWorkInHours { get; }
 
