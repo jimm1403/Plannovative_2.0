22c97f68a7407f22aeb62e3a77bce2dcf5441e34
jimmi <jimmi@DESKTOP-2OTI0T5>
2017-06-26 14:31:26 +0200

    ICustomer og Customer og Job uden noget i.

---
+++ Business_Domain/DomainModels/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business_Domain.DomainModels
+{
+    public class Customer : ICustomer
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string Zip { get; set; }
+        public string City { get; set; }
+        public string CVR { get; set; }
+
+        public Customer(string name, string email, string phone, string address, string zip, string city, string cvr)
+        {
+            Name = name;
+            Email = email;
+            Phone = phone;
+            Address = address;
+            Zip = zip;
+            City = city;
+            CVR = cvr;
+        }
+    }
+}
---
+++ Business_Domain/DomainModels/ICustomer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business_Domain.DomainModels
+{
+    public interface ICustomer
+    {
+        string Name { get; set; }
+        string Email { get; set; }
+        string Phone { get; set; }
+        string Address { get; set; }
+        string Zip { get; set; }
+        string City { get; set; }
+        string CVR { get; set; }
+    }
+}
